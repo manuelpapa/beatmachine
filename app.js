df799aaba96358a352ef19fbd50a951c2923d4a7
manuelpapa <manulito@de-vio.com>
2020-11-05 09:23:42 +0100

    Pad plays sound when clicked

--- app.js
+++ app.js
@@ -17,10 +17,28 @@ class DrumKit {
     //Loop over the pads
     activeBars.forEach((bar) => {
       bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      //Check if bars are active
+      if (bar.classList.contains("active")) {
+        //Check each sound
+        if (bar.classList.contains("kick-pad")) {
+          //Restart time of audio
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
     });
     this.index++;
   }
   start() {
+    console.log(this);
     const interval = (60 / this.bpm) * 1000;
     setInterval(() => {
       this.repeat();
@@ -32,7 +50,7 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
-  pad.addEventListener("animationend", function () {
+  pad.addEventListener("animated", function () {
     this.style.animation = "";
   });
 });
